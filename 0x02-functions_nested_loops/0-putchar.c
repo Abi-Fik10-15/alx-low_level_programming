137f13a7b6bbfc7b9961f3a8247a0e4a0490974d
Abi-Fik 10-15 <99613677+Abi-Fik10-15@users.noreply.github.com>
2022-03-16 21:25:18 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
   #include "main.h"
 
   /** 
-   * main - prints _putchar, followed by a new line
+   * main - prints _putchar
    * Return: Always 0 (Success) 
    */
   int main(void)
   {	
-          _putchar('_');	
-          _putchar('p');	
-          _putchar('u');	
-          _putchar('t');	
-          _putchar('c');	
-          _putchar('h');	
-          _putchar('a');	
-          _putchar('r');	
-          _putchar('\n');
+         char *c = "_putchar";	
+
+         while (*c != '\0')
+         {
+                _putchar(*c);
+                c++;
+         }
+         _putchar('\n');
 
           return (0);
   }
